357cb60bdc3dbdeb66e78e3e5a7c2d9ab73b0dc5
Jdgreiner <jdgreiner@nollou.com>
2024-08-07 14:40:11 -0400

    wip

---
+++ structs/shapes.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+func main() {
+	fmt.Println(Perimeter(Rectangle{10.0, 10.0}))
+	fmt.Println(Perimeter(Rectangle{12.0, 6.0}))
+}
